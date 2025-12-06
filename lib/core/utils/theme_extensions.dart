4e62925076134eb8b7872050515e3a70853b29ec
ibrahim-raad <ibrahim00raad@gmail.com>
2025-12-06 21:49:58 +0200

    added additional color properties to ThemeContextX extension for improved theme management

--- lib/core/utils/theme_extensions.dart
+++ lib/core/utils/theme_extensions.dart
@@ -4,4 +4,19 @@ import '../theme/app_colors.dart';
 extension ThemeContextX on BuildContext {
   ThemeData get theme => Theme.of(this);
   AppColors get colors => theme.extension<AppColors>()!;
+  Color get primary => theme.colorScheme.primary;
+  Color get onPrimary => theme.colorScheme.onPrimary;
+  Color get secondary => theme.colorScheme.secondary;
+  Color get onSecondary => theme.colorScheme.onSecondary;
+  Color get error => theme.colorScheme.error;
+  Color get onError => theme.colorScheme.onError;
+  Color get surface => theme.colorScheme.surface;
+  Color get onSurface => theme.colorScheme.onSurface;
+  Color get surfaceContainer => theme.colorScheme.surfaceContainer;
+  Color get surfaceContainerHighest =>
+      theme.colorScheme.surfaceContainerHighest;
+  Color get onSurfaceVariant => theme.colorScheme.onSurfaceVariant;
+  Color get outline => theme.colorScheme.outline;
+  Color get tertiary => theme.colorScheme.tertiary;
+  Color get onTertiary => theme.colorScheme.onTertiary;
 }
